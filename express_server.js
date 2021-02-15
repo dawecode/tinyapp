fe8299a3995eafa1f191d0f2bbd8ff3c7f8c2763
Catherine Dawe <dawe.cm@gmail.com>
2021-02-15 17:04:52 -0500

    new form + post

--- express_server.js
+++ express_server.js
@@ -1,9 +1,12 @@
 const express = require("express");
 const app = express();
 const PORT = 8080; // default port 8080
-
-
+//ejs
 app.set("view engine", "ejs");
+//body parser
+const bodyParser = require("body-parser");
+app.use(bodyParser.urlencoded({extended: true}));
+
 
 const urlDatabase = {
   "b2xVn2": "http://www.lighthouselabs.ca",
@@ -17,6 +20,15 @@ app.get("/", (req, res) => {
 app.get("/urls", (req, res) => {
   const templateVars = { urls: urlDatabase };
   res.render("urls_index", templateVars);
+});
+
+app.post("/urls", (req, res) => {
+  console.log(req.body);  // Log the POST request body to the console
+  res.send("Ok");         // Respond with 'Ok' (we will replace this)
+});
+
+app.get("/urls/new", (req, res) => {
+  res.render("urls_new");
 });
 
 app.get("/urls/:shortURL", (req, res) => {
