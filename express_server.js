2efad0d67fd5e0f81922167f542492f732815839
Catherine Dawe <dawe.cm@gmail.com>
2021-02-17 13:31:15 -0500

    add helper functions + login form

--- express_server.js
+++ express_server.js
@@ -1,14 +1,18 @@
+
+// how we generate random strings 
 function generateRandomString() {
   const alphaNumerical = '0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ';
-    var result = '';
-    for (let i = 0; i < 7; i++) {
+  var result = '';
+  for (let i = 0; i < 7; i++) {
     result += alphaNumerical.charAt(Math.floor(Math.random() * alphaNumerical.length));
-    }
-    return result;
-}
+  }
+  return result;
+};
 
+//express
 const express = require("express");
 const app = express();
+//port
 const PORT = 8080; // default port 8080
 //ejs
 app.set("view engine", "ejs");
@@ -19,10 +23,34 @@ app.use(bodyParser.urlencoded({extended: true}));
 const cookieParser = require('cookie-parser');
 app.use(cookieParser());
 
+// url Database 
 const urlDatabase = {
   "b2xVn2": "http://www.lighthouselabs.ca",
   "9sm5xK": "http://www.google.com"
 };
+
+// user database 
+const users = { 
+  "userRandomID": {
+    id: "userRandomID", 
+    email: "user@example.com", 
+    password: "purple-monkey-dinosaur"
+  },
+ "user2RandomID": {
+    id: "user2RandomID", 
+    email: "user2@example.com", 
+    password: "dishwasher-funk"
+  }
+};
+
+
+//login form 
+app.get("/login", (req, res) => {
+  const templateVars = { 
+  username: req.cookies["username"]
+  };
+  res.render("urls_login",templateVars);
+});
 
 //register form 
 app.get("/register", (req, res) => {
@@ -55,6 +83,7 @@ app.get("/urls/new", (req, res) => {
   res.render("urls_new",templateVars);
 });
 
+//edit / show tiny url 
 app.get("/urls/:shortURL", (req, res) => {
   const templateVars = {
     shortURL: req.params.shortURL,
@@ -65,9 +94,30 @@ app.get("/urls/:shortURL", (req, res) => {
 });
 
 
-app.get("/urls.json", (req, res) => {
+/*app.get("/urls.json", (req, res) => {
   res.json(urlDatabase);
+});*/
+
+// generate new id and store registration infos 
+app.post("/register", (req, res) => {
+  const newUserID = generateRandomString();
+  const email = req.body.email
+  const password = req.body.password;
+  const user = {
+    id : newUserID,
+    email : email,
+    password : password
+  }; 
+  users[newUserID] = user;
+  console.log(users);
+
+  if (user.email === "" | user.password === ""){
+    res.send("400 Bad Request ")
+  }
+  res.cookie("user_id", newUserID);
+  res.redirect("/urls");         
 });
+
 
 
 app.post("/urls/:id", (req, res) => {
@@ -77,13 +127,14 @@ app.post("/urls/:id", (req, res) => {
   res.redirect('/urls');
 })
 
+//generate random shurt url + add to database 
 app.post("/urls", (req, res) => {
   console.log(req)
   const shortURL = generateRandomString();
-  const longURL= req.body.longURL
-  urlDatabase[shortURL]= longURL
-  console.log(urlDatabase)
-  res.redirect(`/urls/${shortURL}`);         
+  const longURL= req.body.longURL;
+  urlDatabase[shortURL]= longURL;
+  console.log(urlDatabase);
+  res.redirect("/urls/${shortURL}");         
 });
 
 
@@ -91,19 +142,19 @@ app.post("/urls/:shortURL/delete", (req, res) => {
   console.log("DELETE ROUTE HAS BEEN HIT");
   console.log(req.params.shortURL);
   delete urlDatabase[req.params.shortURL];
-  res.redirect('/urls');
+  res.redirect("/urls");
 })
 
 app.post("/login", (req, res) => {
-  let cookie = req.body.username
+  let cookie = req.body.username;
   res.cookie("username",cookie);
-  res.redirect('/urls');
+  res.redirect("/urls");
 })
 
 
 app.post("/logout", (req, res) => {
   res.clearCookie('username');
-  res.redirect('/urls');
+  res.redirect("/urls");
 })
 
 
