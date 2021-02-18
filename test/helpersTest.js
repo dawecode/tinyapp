0992307effd07c57d608ece67f9ebacc31dd3c83
Catherine Dawe <dawe.cm@gmail.com>
2021-02-18 17:16:06 -0500

    cleaned up helper.js

--- test/helpersTest.js
+++ test/helpersTest.js
@@ -18,7 +18,7 @@ const testUsers = {
 describe('getUserByEmail', function() {
   it('should return a user with valid email', function() {
     const user = getUserByEmail("user@example.com", testUsers);
-    const expectedOutput = "userRandomID"
+    const expectedOutput = "userRandomID";
     assert.equal(user.id,expectedOutput);
   });
 
